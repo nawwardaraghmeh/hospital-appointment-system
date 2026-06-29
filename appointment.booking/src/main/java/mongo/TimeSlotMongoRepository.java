193f86f1d3e0c90ef254669a28e9c7cf60de3b50
nawwardaraghmeh <daraghmehnawwar@gmail.com>
2026-06-29 22:09:38 +0200

    implemented TimeSlotMongoRepository.findAll() with MongoDB support - tests pass

--- appointment.booking/src/main/java/mongo/TimeSlotMongoRepository.java
+++ appointment.booking/src/main/java/mongo/TimeSlotMongoRepository.java
@@ -1,20 +1,40 @@
 package mongo;
 
-
 import models.TimeSlot;
 import repositories.TimeSlotRepository;
 
-import java.util.Collections;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.bson.Document;
+import com.mongodb.client.MongoCollection;
 
 public class TimeSlotMongoRepository implements TimeSlotRepository {
 
+    private final MongoCollection<Document> timeSlotCollection;
+
     public TimeSlotMongoRepository(com.mongodb.MongoClient client, 
                                    String databaseName, String collectionName) {
+        this.timeSlotCollection = client.getDatabase(databaseName)
+                                        .getCollection(collectionName);
     }
 
     @Override
     public List<TimeSlot> findAll() {
-        return Collections.emptyList();
+        return StreamSupport.stream(timeSlotCollection.find().spliterator(), false)
+                .map(this::fromDocumentToTimeSlot)
+                .collect(Collectors.toList());
+    }
+
+    private TimeSlot fromDocumentToTimeSlot(Document doc) {
+        return new TimeSlot(
+                doc.getString("id"),
+                doc.getString("doctorName"),
+                doc.getString("department"),
+                doc.getString("roomNumber"),
+                LocalDateTime.parse(doc.getString("appointmentDateTime"))
+        );
     }
 }

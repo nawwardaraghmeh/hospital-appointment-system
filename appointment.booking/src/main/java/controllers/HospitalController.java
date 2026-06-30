6ef9f76ba13c314916cce77a2e3ad062edf2b7c4
nawwardaraghmeh <daraghmehnawwar@gmail.com>
2026-06-30 16:19:10 +0200

    added base HospitalController

--- appointment.booking/src/main/java/controllers/HospitalController.java
+++ appointment.booking/src/main/java/controllers/HospitalController.java
@@ -1,5 +1,21 @@
 package controllers;
 
+import models.Appointment;
+import models.TimeSlot;
+import repositories.AppointmentRepository;
+import repositories.TimeSlotRepository;
+
+import java.util.Collections;
+import java.util.List;
+
 public class HospitalController {
 
+    private final TimeSlotRepository timeSlotRepository;
+    private final AppointmentRepository appointmentRepository;
+
+    public HospitalController(TimeSlotRepository timeSlotRepository, 
+                              AppointmentRepository appointmentRepository) {
+        this.timeSlotRepository = timeSlotRepository;
+        this.appointmentRepository = appointmentRepository;
+    }
 }

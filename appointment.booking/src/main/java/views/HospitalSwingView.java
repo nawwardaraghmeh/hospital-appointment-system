f354703e061fdb8b9bad6769a989751756aa244d
nawwardaraghmeh <daraghmehnawwar@gmail.com>
2026-07-01 10:10:51 +0200

    added time slot jlist to view - test passes

--- appointment.booking/src/main/java/views/HospitalSwingView.java
+++ appointment.booking/src/main/java/views/HospitalSwingView.java
@@ -5,6 +5,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,6 +29,7 @@ public class HospitalSwingView extends JFrame implements HospitalView {
     private HospitalController controller;
     
     private JList<String> timeSlotList;
+    private DefaultListModel<String> timeSlotListModel;
     private JList<String> appointmentList;
     private JTextField patientNameTextField;
     private JButton bookButton;
@@ -44,6 +46,30 @@ public class HospitalSwingView extends JFrame implements HospitalView {
     }
 
     private void initComponents() {
+    		JPanel mainPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.BOTH;
+        
+        timeSlotListModel = new DefaultListModel<>();
+        timeSlotList = new JList<>(timeSlotListModel);
+        timeSlotList.setName("timeSlotList"); 
+        JScrollPane timeSlotScrollPane = new JScrollPane(timeSlotList);
+        timeSlotScrollPane.setName("timeSlotScrollPane");
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 1.0;
+        gbc.weighty = 0.5;
+        mainPanel.add(new JLabel("Available Time Slots"), gbc);
+        
+        gbc.gridy = 1;
+        gbc.weighty = 1.0;
+        mainPanel.add(timeSlotScrollPane, gbc);
+        
+        add(mainPanel);
     }
 
     public void setHospitalController(HospitalController controller) {
